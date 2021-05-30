a2a244787b3e0e848bff70c5d433ba31a1b3ad3e
Artem Rakov <scoff1k@gmail.com>
2021-05-29 23:18:32 -0400

    added commander

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log('Hello');
+import { program } from 'commander';
+
+program.version('0.0.1');
+
+program
+  .arguments('<url>')
+  .description('use page loader to save web pages locally')
+  .option('-o, --output [dir]', 'output dir', '/app')
+  .action((url) => {
+    console.log(url);
+  });
+
+program.parse(process.argv);
