f04d6977218d82eab0007911603551306a98037e
Artem Rakov <scoff1k@gmail.com>
2021-06-09 03:02:15 +0000

    added cheerio

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -18,13 +18,13 @@ const createTempDir = () => fs.mkdtempSync(path.join(os.tmpdir(), 'path-loader-'
 
 beforeEach(() => {
   // cases.forEach(createTempDir);
-  nock.disableNetConnect();
+  // nock.disableNetConnect();
 });
 
 afterEach(() => {
   // cases.forEach((pathToFile) => fs.rmdirSync(pathToTempDir(pathToFile)));
   nock.cleanAll();
-  nock.enableNetConnect();
+  // nock.enableNetConnect();
 });
 
 test('loads html', async () => {
@@ -44,5 +44,26 @@ test('loads html', async () => {
 });
 
 test('loads html with assets', async () => {
+  const outputDir = createTempDir();
+  const url = new URL('https://ru.hexlet.io/courses');
+  const outputFilePath = path.join(outputDir, 'ru-hexlet-io-courses.html');
+  const initialHtml = readFile('initial/ru-hexlet-io-courses.html');
+  const image = readFile('result/ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png');
 
-})
+  nock(url.origin)
+    .get(url.pathname)
+    .reply(200, initialHtml);
+
+  nock(url.origin)
+    .get('/assets/professions/nodejs.png')
+    .reply(200, image);
+
+  await pathLoader(url.href, outputDir);
+
+  const outputFile = fs.readFileSync(outputFilePath, 'utf-8');
+  const expectedHtml = readFile('result/ru-hexlet-io-courses.html');
+  const imagePath = getFixturePath('result/ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png');
+
+  expect(outputFile).toEqual(expectedHtml);
+  expect(() => fs.statSync(imagePath)).not.toThrowError();
+});
--- src/page-loader.js
+++ src/page-loader.js
@@ -2,17 +2,31 @@
 import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
+import cheerio from 'cheerio';
 
 // remember about path.format
 const generateFilepath = (str, ext = '') => str.replace(/[^a-zA-Z0-9]/g, '-') + ext;
 
-const pageLoader = async (u, outputPath) => {
+const extractAssets = async (html, url) => {
+  const $ = cheerio.load(html);
+  const sources = $('img').map((_, el) => $(el).attr('src')).toArray();
+
+  const assetsPromises = sources.map((src) => axios.get(url.origin + src, { responseType: 'stream' }));
+  const assetsResponses = await Promise.all(assetsPromises);
+  return assetsResponses.map((assetResponse) => assetResponse.data);
+}
+
+const pageLoader = async (u, outputDir) => {
   const url = new URL(u);
   const response = await axios.get(url.href);
 
-  const rootHtmlPath = generateFilepath(url.hostname + url.pathname, '.html');
+  const assets = await extractAssets(response.data, url)
+  console.log(assets);
+  // const state = buildState(html, url)
 
-  fs.writeFileSync(path.join(outputPath, rootHtmlPath), response.data);
+  // const rootHtmlPath = generateFilepath(url.hostname + url.pathname, '.html');
+
+  // fs.writeFileSync(path.join(outputPath, rootHtmlPath), response.data);
 };
 
 export default pageLoader;
