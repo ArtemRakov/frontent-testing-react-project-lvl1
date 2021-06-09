e3e6efaba5f9301becf20a462ecbc4e871d9a7da
Artem Rakov <scoff1k@gmail.com>
2021-06-09 17:37:05 +0000

    lint fix

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import buildState from './src/state.js';
 import findAssets from './src/assets.js';
 import loadFiles from './src/loadFiles.js';
 
-
 const pageLoader = async (u, outputDir) => {
   const url = new URL(u);
   const response = await axios.get(url.href);
