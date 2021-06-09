51698be9e334ad88f16d5d1d268bad4304d74aa1
Artem Rakov <scoff1k@gmail.com>
2021-06-09 05:47:53 +0000

    fix assets

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,37 +14,35 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 const createTempDir = () => fs.mkdtempSync(path.join(os.tmpdir(), 'path-loader-'));
 
-// const cases = ['plain-html'];
+let outputDir;
 
 beforeEach(() => {
-  // cases.forEach(createTempDir);
-  // nock.disableNetConnect();
+  outputDir = createTempDir();
+  nock.disableNetConnect();
 });
 
 afterEach(() => {
-  // cases.forEach((pathToFile) => fs.rmdirSync(pathToTempDir(pathToFile)));
+  fs.rmSync(outputDir, { recursive: true, force: true });
   nock.cleanAll();
-  // nock.enableNetConnect();
+  nock.enableNetConnect();
 });
 
 test('loads html', async () => {
-  const outputPath = createTempDir();
   const url = new URL('http://example.com/test');
-  const outputFilePath = path.join(outputPath, 'example-com-test.html');
+  const outputFilePath = path.join(outputDir, 'example-com-test.html');
   const exampleHtml = readFile('example-com-test.html');
 
   nock(url.origin)
     .get(url.pathname)
     .reply(200, exampleHtml);
 
-  await pathLoader(url.href, outputPath);
+  await pathLoader(url.href, outputDir);
 
   const outputFile = fs.readFileSync(outputFilePath, 'utf-8');
   expect(outputFile).toEqual(exampleHtml);
 });
 
 test('loads html with assets', async () => {
-  const outputDir = createTempDir();
   const url = new URL('https://ru.hexlet.io/courses');
   const outputFilePath = path.join(outputDir, 'ru-hexlet-io-courses.html');
   const initialHtml = readFile('initial/ru-hexlet-io-courses.html');
--- src/page-loader.js
+++ src/page-loader.js
@@ -1,32 +1,73 @@
 // import _ from 'lodash';
 import axios from 'axios';
 import fs from 'fs';
+import fsPromises from 'fs/promises';
 import path from 'path';
 import cheerio from 'cheerio';
 
 // remember about path.format
 const generateFilepath = (str, ext = '') => str.replace(/[^a-zA-Z0-9]/g, '-') + ext;
 
-const extractAssets = async (html, url) => {
-  const $ = cheerio.load(html);
-  const sources = $('img').map((_, el) => $(el).attr('src')).toArray();
+const buildState = (url, assetSources) => {
+  const { origin, hostname, pathname } = url;
+  const root = generateFilepath(hostname + pathname);
+  const assetsDir = `${root}_files`;
+  const assertPrefix = generateFilepath(hostname);
 
-  const assetsPromises = sources.map((src) => axios.get(url.origin + src, { responseType: 'stream' }));
+  const assets = assetSources.map((src) => {
+    const { ext, dir, name } = path.parse(src);
+    const filePathWithoutExt = path.join(dir, name);
+    const newSrc = path.join(assetsDir, assertPrefix + generateFilepath(filePathWithoutExt, ext));
+
+    return {
+      oldSrc: src,
+      originSrc: origin + src,
+      newSrc,
+    };
+  });
+
+  return {
+    origin,
+    assets,
+    htmlName: generateFilepath(root, '.html'),
+    assetsDir,
+  };
+};
+
+const extractAssets = async (sources) => {
+  const assetsPromises = sources.map(({ originSrc }) => axios.get(originSrc, { responseType: 'stream' }));
   const assetsResponses = await Promise.all(assetsPromises);
   return assetsResponses.map((assetResponse) => assetResponse.data);
-}
+};
 
 const pageLoader = async (u, outputDir) => {
   const url = new URL(u);
   const response = await axios.get(url.href);
+  const html = response.data;
 
-  const assets = await extractAssets(response.data, url)
-  console.log(assets);
-  // const state = buildState(html, url)
+  const $ = cheerio.load(html);
+  const assetSources = $('img').map((_, el) => $(el).attr('src')).toArray();
 
-  // const rootHtmlPath = generateFilepath(url.hostname + url.pathname, '.html');
+  const state = buildState(url, assetSources);
 
-  // fs.writeFileSync(path.join(outputPath, rootHtmlPath), response.data);
+  if (state.assets.length > 0) {
+    state.assets.forEach((asset) => {
+      const img = $(`img[src='${asset.oldSrc}']`);
+      img.attr('src', asset.newSrc);
+    });
+
+    fs.mkdirSync(path.join(outputDir, state.assetsDir));
+    const assetFiles = await extractAssets(state.assets, state.origin);
+
+    const creationPromises = state.assets.map((asset) => {
+      const data = assetFiles.find((file) => file.responseUrl === asset.originSrc);
+      return fsPromises.writeFile(path.join(outputDir, asset.newSrc), data);
+    });
+
+    await Promise.all(creationPromises);
+  }
+
+  fs.writeFileSync(path.join(outputDir, state.htmlName), $.html());
 };
 
 export default pageLoader;
