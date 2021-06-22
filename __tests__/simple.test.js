509546b378da81d7495f32e2b7b1c199ec6186ee
Artem Rakov <scoff1k@gmail.com>
2021-06-22 18:05:33 -0400

    downgrade so could work with babel

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -1,13 +1,8 @@
-import { test, expect } from '@jest/globals';
-import { fileURLToPath } from 'url';
 import nock from 'nock';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
 import pathLoader from '../index.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
