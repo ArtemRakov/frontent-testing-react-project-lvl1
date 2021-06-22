aca76007f403e532dbc483251465ba0c67f28326
Artem Rakov <scoff1k@gmail.com>
2021-06-22 23:32:18 +0000

    fixes

--- __tests__/withAssets.test.js
+++ __tests__/withAssets.test.js
@@ -1,8 +1,12 @@
+import { fileURLToPath } from 'url';
 import nock from 'nock';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
 import pathLoader from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
